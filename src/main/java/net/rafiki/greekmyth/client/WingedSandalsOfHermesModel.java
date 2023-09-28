4f85c30e5ed093450bbfeec940f95b75c89b3a78
M8SON <masonmisch77@gmail.com>
2023-09-27 23:46:08 -0500

    3D Render in... Need to change pivot points in blockbench, and get animation right in the custom item java folder then they should work.

---
+++ src/main/java/net/rafiki/greekmyth/client/WingedSandalsOfHermesModel.java
@@ -0,0 +1,23 @@
+package net.rafiki.greekmyth.client;
+
+import net.minecraft.resources.ResourceLocation;
+import net.rafiki.greekmyth.GreekMyth;
+import software.bernie.geckolib.core.animatable.GeoAnimatable;
+import software.bernie.geckolib.model.GeoModel;
+
+public class WingedSandalsOfHermesModel extends GeoModel {
+    @Override
+    public ResourceLocation getModelResource(GeoAnimatable animatable) {
+        return new ResourceLocation(GreekMyth.MOD_ID, "geo/wingedsandalsofhermesmodel.geo.json");
+    }
+
+    @Override
+    public ResourceLocation getTextureResource(GeoAnimatable animatable) {
+        return new ResourceLocation(GreekMyth.MOD_ID, "textures/models/armor/wingedsandalsofhermestexture.png");
+    }
+
+    @Override
+    public ResourceLocation getAnimationResource(GeoAnimatable animatable) {
+        return new ResourceLocation(GreekMyth.MOD_ID, "animations/wingedsandalsofhermesmodel.animation.json");
+    }
+}
