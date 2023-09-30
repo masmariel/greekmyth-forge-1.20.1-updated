c6a2718e450e262c240d532d6d3457ffe26e3d1f
Rxfiki <mc.espinoza.me@gmail.com>
2023-09-30 13:52:08 -0500

    v0.0.19 Added:
    -WIP:
    Lyre Of Apollo:
    Plays music, but does not stop when right clicked again

--- src/main/java/net/rafiki/greekmyth/item/ModItems.java
+++ src/main/java/net/rafiki/greekmyth/item/ModItems.java
@@ -34,17 +34,17 @@ public class ModItems {
 
     //CUSTOM
     public static final RegistryObject<Item> LYRE_OF_APOLLO = ITEMS.register("lyre_of_apollo",
-            () -> new LyreOfApolloItem(new Item.Properties()));
+            () -> new LyreOfApolloItem(new Item.Properties().rarity(Rarity.RARE)));
     public static final RegistryObject<Item> SWORD_OF_ARES = ITEMS.register("sword_of_ares",
-            () -> new SwordOfAresItem(ModToolTiers.ADAMANTINE, 3, -3.2f,new Item.Properties().durability(450)));
+            () -> new SwordOfAresItem(ModToolTiers.ADAMANTINE, 3, -3.2f,new Item.Properties().durability(450).rarity(Rarity.EPIC).setNoRepair()));
     public static final RegistryObject<Item> WINGED_SANDALS_OF_HERMES = ITEMS.register("winged_sandals_of_hermes",
-            () -> new WingedSandalsOfHermesItem(ModArmorMaterials.WINGED_SANDALS_OF_HERMES, ArmorItem.Type.BOOTS, new Item.Properties()));
+            () -> new WingedSandalsOfHermesItem(ModArmorMaterials.WINGED_SANDALS_OF_HERMES, ArmorItem.Type.BOOTS, new Item.Properties().defaultDurability(100).rarity(Rarity.EPIC).setNoRepair()));
     public static final RegistryObject<Item> HELM_OF_DARKNESS = ITEMS.register("helm_of_darkness",
-            () -> new HelmOfDarknessItem(ModArmorMaterials.HELM_OF_DARKNESS, ArmorItem.Type.HELMET, new Item.Properties()));
+            () -> new HelmOfDarknessItem(ModArmorMaterials.HELM_OF_DARKNESS, ArmorItem.Type.HELMET, new Item.Properties().defaultDurability(300).rarity(Rarity.EPIC).setNoRepair()));
     public static final RegistryObject<Item> TRIDENT_OF_POSEIDON = ITEMS.register("trident_of_poseidon",
-            () -> new TridentOfPoseidonItem(new Item.Properties().durability(400)));
+            () -> new TridentOfPoseidonItem(new Item.Properties().defaultDurability(400).rarity(Rarity.EPIC).setNoRepair()));
     public static final RegistryObject<Item> BIDENT_OF_HADES = ITEMS.register("bident_of_hades",
-            () -> new BidentOfHadesItem(new Item.Properties().durability(400)));
+            () -> new BidentOfHadesItem(new Item.Properties().defaultDurability(400).rarity(Rarity.EPIC).setNoRepair()));
 
     //FUEL
     public static final RegistryObject<Item> FORGE_COAL = ITEMS.register("forge_coal",
